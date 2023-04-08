dd6b10ca3931e98c3ff1016d1926414887ce1781
Chamil <kamilgazizov09@gmail.com>
2023-04-09 00:33:20 +0300

    wad

--- FlutterApi/Program.cs
+++ FlutterApi/Program.cs
@@ -20,33 +20,3 @@ namespace FlutterApi
             });
     }
 }
-
-//var builder = WebApplication.CreateBuilder(args);
-
-//// Add services to the container.
-
-//builder.Services.AddControllers();
-//// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-//builder.Services.AddEndpointsApiExplorer();
-//builder.Services.AddSwaggerGen();
-
-//builder.Services.AddDbContext<FlutterApiDB>(options => options.UseInMemoryDatabase("FlutterDb"));
-////builder.Services.AddDbContext<FlutterApiDB>(options =>
-////    options.UseSqlServer(builder.Configuration.GetConnectionString("FlutterApiConnectionString")));
-
-//var app = builder.Build();
-
-//// Configure the HTTP request pipeline.
-//if (app.Environment.IsDevelopment())
-//{
-//    app.UseSwagger();
-//    app.UseSwaggerUI();
-//}
-
-////app.UseHttpsRedirection();
-
-//app.UseAuthorization();
-
-//app.MapControllers();
-
-//app.Run();
